9b7c1e700624552dfc9de0dc08a28542e93e90cc
PaulDeveloper73 <kisakyepaul73@gmail.com>
2024-07-27 14:39:23 +0300

    Min edits

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,7 +1,7 @@
 import { FaPlus } from "react-icons/fa";
 import { useRef } from "react";
 
-function AddItem({newItem, setNewItem, handleSubmit}) {
+function AddItem({ newItem, setNewItem, handleSubmit }) {
   const inputRef = useRef();
 
   return (
@@ -15,9 +15,13 @@ function AddItem({newItem, setNewItem, handleSubmit}) {
         placeholder="Add Item"
         required
         value={newItem}
-        onChange={(e)=>setNewItem(e.target.value)}
+        onChange={(e) => setNewItem(e.target.value)}
       />
-      <button type='submit' aria-label="Add Item" onClick={()=>inputRef.current.focus()}>
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
         <FaPlus />
       </button>
     </form>
